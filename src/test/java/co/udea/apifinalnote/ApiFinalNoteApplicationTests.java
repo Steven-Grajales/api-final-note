464d9a620165dfd8d3ce1131bd1d3e8d9501eb10
Steven <steven.deibi@hotmail.com>
2020-10-27 22:11:56 -0500

    creación de la estructura del proyecto backend final-note

---
+++ src/test/java/co/udea/apifinalnote/ApiFinalNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udea.apifinalnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinalNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
