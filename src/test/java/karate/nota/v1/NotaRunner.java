8cd6b62b74b53cf4b2198eb3890a7d6604245267
manaranjoc <angel.naranjo@udea.edu.co>
2020-11-03 10:41:49 -0500

    Refactor test to multiple Scenarios

--- src/test/java/karate/nota/v1/NotaRunner.java
+++ src/test/java/karate/nota/v1/NotaRunner.java
@@ -1,12 +1,13 @@
 package karate.nota.v1;
 
+import co.udea.apifinalnote.ApiFinalNoteApplication;
 import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes= ApiFinalNoteApplication.class)
 public class NotaRunner {
 
     @Karate.Test
--- src/test/java/karate/nota/v1/create.feature
+++ src/test/java/karate/nota/v1/create.feature
@@ -4,16 +4,16 @@ Feature: Creation of notas
     * url baseUrl
     * def notaPath = '/api/v1/nota'
 
-  Scenario: Create a nota
-    * def notaToCreate =
-    """
-    {
-    "nota": 5
-    }
-    """
+  Scenario Outline: Create a nota
     Given path notaPath
     And request notaToCreate
     When method post
     Then status 201
     And match responseHeaders contains {Location:[#string]}
+    Examples:
+      | nota | porcentaje |
+      | 5    | 0.2        |
+      | 2    | 0.3        |
+      | 3.5  | 0.4        |
+      | 4.5  | 0.1        |
 
