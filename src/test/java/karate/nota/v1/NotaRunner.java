9a61d8ddcd93e5204d7446bf906c7722523c4037
manaranjoc <angel.naranjo@udea.edu.co>
2020-11-03 10:03:18 -0500

    Init spring previous to karate tests

--- src/test/java/karate/nota/v1/NotaRunner.java
+++ src/test/java/karate/nota/v1/NotaRunner.java
@@ -1,7 +1,12 @@
 package karate.nota.v1;
 
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class NotaRunner {
 
     @Karate.Test
