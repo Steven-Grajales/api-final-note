314c94be46b3b28b66a32066c626c23f40127897
manaranjoc <angel.naranjo@udea.edu.co>
2020-11-08 17:20:30 -0500

    Add error messages

---
+++ src/main/java/co/udea/apifinalnote/component/shared/model/ErrorMessage.java
@@ -0,0 +1,28 @@
+package co.udea.apifinalnote.component.shared.model;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Generated
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private String exception;
+
+    private String message;
+
+    private String path;
+
+    private LocalDateTime date;
+
+    public ErrorMessage(Exception exception, String path) {
+        this.exception = exception.getClass().getName();
+        this.message = exception.getMessage();
+        this.path = path;
+        this.date = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/co/udea/apifinalnote/component/shared/web/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package co.udea.apifinalnote.component.shared.web.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    private static final String DESCRIPTION = "Bad Request Exception (400)";
+
+    public BadRequestException(String detail){
+        super(DESCRIPTION + ". " + detail);
+    }
+}
